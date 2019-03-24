a2b8fea6c8f8e95bd11af5b4a32b2dfd9f7962e5
Anton Bakatovich <antbakatovich@gmail.com>
2019-03-24 17:53:33 +0300

    Add draw method in Ellipce.

--- Lab_01/src/figures/shape2d/ellipse/Ellipse.java
+++ Lab_01/src/figures/shape2d/ellipse/Ellipse.java
@@ -12,13 +12,25 @@ public class Ellipse extends Shape2D {
 
 	public Ellipse(Point center, Point firstPoint, Point secondPoint, Color stroke, Color fill) {
 		super(center, stroke, fill);
-		this.firstPoint = firstPoint;
-		this.secondPoint = secondPoint;
+
+		this.firstPoint = new Point(
+			Math.min(firstPoint.x, secondPoint.x),
+			Math.min(firstPoint.y, secondPoint.y)
+		);
+		this.secondPoint = new Point(
+			Math.max(firstPoint.x, secondPoint.x),
+			Math.max(firstPoint.y, secondPoint.y)
+		);
 	}
 
 	@Override
 	public void draw(Pane pane) {
-
+		javafx.scene.shape.Ellipse ellipse = new javafx.scene.shape.Ellipse();
+		ellipse.setCenterX(center.x);
+		ellipse.setCenterY(center.y);
+		ellipse.setRadiusX(secondPoint.x - center.x);
+		ellipse.setRadiusY(firstPoint.y - center.y);
+		pane.getChildren().add(ellipse);
 	}
 
 	public void move(Point point) {
